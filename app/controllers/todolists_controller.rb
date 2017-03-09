6946ea3c4e63d74cc179bf4976edda91a9e2c8ad
LYC <LYC@LYCdeMacBook-Air.local>
2017-03-09 12:13:01 +0800

    private

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -25,9 +25,16 @@ class TodolistsController < ApplicationController
 
 	def update
 		@todolist = Todolist.find(params[:id])
-		@todolist.update(params[:id])
+		@todolist.update(todolist_params)
 
 		redirect_to todolist_path(@todolist)
+	end
+
+
+	private 
+
+	def todolist_params
+		params.require(:todolist).permit(:title,:duedate,:description)
 	end
 
 end
